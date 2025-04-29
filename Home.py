6a3cd105514258626ef473e46e6e174ba91d2ad4
Yarid Tyran <yarid.tyran@uconn.edu>
2025-04-29 19:02:30 -0400

    update figures labels

--- Home.py
+++ Home.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from anomaly_detection import detect_anomalies
-
-
+import matplotlib.dates as mdates
+from matplotlib.ticker import ScalarFormatter
 
 
 st.set_page_config(page_title="Time Series Anomaly Detection", layout="wide")
@@ -14,7 +14,6 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
-
 def run_detection():
     df = st.session_state["uploaded_df"]
     anomalies, hourly, stl = detect_anomalies(df)
@@ -23,18 +22,13 @@ def run_detection():
     st.session_state["stl"]       = stl
 
 
-
 def reset_app():
     for key in ["anomalies", "hourly", "stl", "uploaded_df", "uploaded_file"]:
         st.session_state.pop(key, None)
 
 
-
 if "anomalies" not in st.session_state:
     st.title("Time Series Anomaly Detection")
-
-
-
     uploaded = st.file_uploader(
         "Upload your time series CSV file",
         type=["csv"],
@@ -42,13 +36,10 @@ if "anomalies" not in st.session_state:
     )
 
     if uploaded is None:
-
         st.stop()
 
     df = pd.read_csv(uploaded)
     st.session_state["uploaded_df"] = df
-
-
 
     st.success("File uploaded successfully")
     st.dataframe(df.head())
@@ -57,9 +48,7 @@ if "anomalies" not in st.session_state:
 
 
 st.title("Time Series Anomaly Detection")
-
 st.button("Upload New File", on_click=reset_app)
-
 st.success("Detection completed")
 
 anomalies = st.session_state["anomalies"]
@@ -95,13 +84,20 @@ for a in anomalies:
         resid    = stl.resid
 
         fig, ax = plt.subplots(figsize=(8, 3))
-        ax.plot(sub["Start Time"], sub["y"], label="Original")
-        ax.plot(sub["Start Time"], trend[sub.index], label="Trend")
-        ax.plot(sub["Start Time"], seasonal[sub.index], label="Seasonal")
-        ax.plot(sub["Start Time"], resid[sub.index], label="Residual")
+        ax.plot(sub["Start Time"], sub["y"] / 1e8, label="Original")
+        ax.plot(sub["Start Time"], trend[sub.index] / 1e8, label="Trend")
+        ax.plot(sub["Start Time"], seasonal[sub.index] / 1e8, label="Seasonal")
+        ax.plot(sub["Start Time"], resid[sub.index] / 1e8, label="Residual")
         ax.axvline(ts, color="red", linestyle="--", label="Anomaly")
 
-        ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
-        ax.set_title(f"Anomaly details: {label}")
+        ax.set_xlabel("Timestamp (MM-DD, HH:MM)", fontsize=10)
+        ax.set_ylabel("Aggregated Value (×10⁸)", fontsize=10)
+        ax.set_title(f"Anomaly details: {label}", fontsize=12, fontweight="bold")
+        ax.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
+
+        ax.xaxis.set_major_formatter(mdates.DateFormatter("%m-%d, %H:%M"))
+        fig.autofmt_xdate(rotation=30)
+
         ax.legend()
         st.pyplot(fig)
+
